978cf028f3ec8dec2c9bfabce37ff52dbfad8940
Andreas Madsack <andreas@madflex.de>
2025-02-14 13:57:17 +0100

    update to 0.17.4

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-version=v0.16.0
-filename=gotosocial_0.16.0_linux_amd64.tar.gz
+version=v0.17.4
+filename=gotosocial_0.17.4_linux_amd64.tar.gz
 
 wget https://github.com/superseriousbusiness/gotosocial/releases/download/${version}/${filename}
 mkdir tmp
